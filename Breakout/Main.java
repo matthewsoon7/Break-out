9ecdbd4cd14e37993ed6198b35f962ab9b4576a6
matthewsoon7 <matthewsoon7@gmail.com>
2017-02-13 01:50:30 -0600

    Add files via upload

---
+++ Breakout/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		JFrame obj = new JFrame();
+		Gameplay gameplay = new Gameplay();
+		obj.setBounds(10,10,700,600);
+		obj.setTitle("#Not Breakout- totally legal");
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
+		
+		
+		
+	}
+	
+
+}
